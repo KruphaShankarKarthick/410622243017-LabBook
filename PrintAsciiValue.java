26939564441c7062a16b7c31552eabb00a788cd3
Krupha Shankar Karthick <116346900+KruphaShankarKarthick@users.noreply.github.com>
2023-06-23 14:41:34 +0530

    Add files via upload

---
+++ PrintAsciiValue.java
@@ -0,0 +1,11 @@
+public class PrintAsciiValue {
+	public static void main(String[] args) {
+		char ch='H';
+		int ascii=ch;
+		//You can Also Cast Char to int
+		int castAscii=(int)ch;
+		System.out.println("The ASCII Value of"+ch+"is:"+ascii);
+		System.out.println("The ASCII value of"+ch+"is:"+castAscii);
+	}
+
+}
